f713ae14b61fc611deb9e1bf345301efebec09fe
jessicacliong <14395@coderacademy.edu.au>
2023-05-12 16:08:33 +1000

    Functioning User input to CSV

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import csv
 import time
+import random
 
 
 def user_sleep_input():
@@ -7,29 +8,27 @@ def user_sleep_input():
   hours_of_sleep = int(input("How many hours did you sleep? "))
   quality_of_sleep = int(input("From a scale of 1 to 10 how would you rate your sleep? "))
   caffeine = input("Did you have any coffee in the afternoon/evening? Enter: (Y/N) ")
-  if input == "YN":
-    raise 
   comments = input("Do you want to enter any other comments? Enter: (Y/N) ")
   if comments == 'N':
     other_comments = ('')
     print("Thank you for using Snooze It. Your data has been saved! ")
-
-
   else:
     other_comments = input("Please enter comments: (Press <Enter> to Continue) ")
     print("Thank you for using Snooze It. Your data has been saved! ")
 
-    file = open('user_information.csv', 'a', newline='')
-    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep', 'Caffeine', 'Comments', 'Other Comments']
-    outputDictWriter = csv.DictWriter(file, headers)
-    outputDictWriter.writeheader()
-    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep': quality_of_sleep, 'Caffeine': caffeine, 'Comments': comments, 'Other Comments': other_comments})
-    file.close()
 
-# def sleep_tip():
+  file = open('user_information.csv', 'a', newline='')
+  headers = ['Date', 'Hours of Sleep', 'Quality of Sleep', 'Caffeine', 'Comments', 'Other Comments']
+  outputDictWriter = csv.DictWriter(file, headers)
+  outputDictWriter.writeheader()
+  outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep': quality_of_sleep, 'Caffeine': caffeine, 'Comments': comments, 'Other Comments': other_comments})
+  file.close()
+
+def sleep_tip():
+  pass
   # write sleeping tips on a separate file
   # generate random sentence from file
   # print(tip)
-
+  
 user_sleep_input()
-  # sleep_tip()
+sleep_tip()
--- src/test_main.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import time
-from main import user_sleep_input
-
-def user_sleep_input():
-    
-
