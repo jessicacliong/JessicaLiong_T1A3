2991c8d59b9c24823a104991c44a237d821b7eeb
jessicacliong <14395@coderacademy.edu.au>
2023-05-14 23:14:55 +1000

    Started Feature 2

--- src/main.py
+++ src/main.py
@@ -1,37 +1,28 @@
 import csv
 import datetime
 import random
-import pandas
+import pandas as pd
 
-# class Input_Variables:
-#   def __init__(self, date, hours_of_sleep, quality_of_sleep, caffeine, comments, other_comments):
-#     self.date = date
-#     self.hours_of_sleep = hours_of_sleep
-#     self.quality_of_sleep = quality_of_sleep
-#     self.caffeine = caffeine
-#     self.comments = comments
-#     self.other_comments = other_comments
+def user_input():
+  year = int(input('Enter a year of the log: '))
+  month = int(input('Enter a month of the log: '))
+  day = int(input('Enter a date of the log: '))
+  date = datetime.date(year, month, day)
+  hours_of_sleep = int(input("How many hours did you sleep? ")) 
+  quality_of_sleep = int(input("From a scale of 1 to 10 how would you rate your sleep? "))
+  caffeine = input("Did you have any coffee in the afternoon/evening? Enter: (Y/N) ")
+  comments = input("Do you want to enter any other comments? Enter: (Y/N) ")
+  if comments == 'N':
+    other_comments = ('')
+    print("Thank you for using Snooze It. Your data has been saved! ")
+  else:
+    other_comments = input("Please enter comments: (Press <Enter> to Continue) ")
+    print("Thank you for using Snooze It. Your data has been saved! ")
 
-# def user_input():
-  # year = int(input('Enter a year of the log: '))
-  # month = int(input('Enter a month of the log: '))
-  # day = int(input('Enter a date of the log: '))
-  # date = datetime.date(year, month, day)
-  # hours_of_sleep = int(input("How many hours did you sleep? "))
-  # quality_of_sleep = int(input("From a scale of 1 to 10 how would you rate your sleep? "))
-  # caffeine = input("Did you have any coffee in the afternoon/evening? Enter: (Y/N) ")
-  # comments = input("Do you want to enter any other comments? Enter: (Y/N) ")
-  # if comments == 'N':
-  #   other_comments = ('')
-  #   print("Thank you for using Snooze It. Your data has been saved! ")
-  # else:
-  #   other_comments = input("Please enter comments: (Press <Enter> to Continue) ")
-  #   print("Thank you for using Snooze It. Your data has been saved! ")
-
-# def write_user_input(user_input):
-#   with open('user_information.csv', 'a', newline='') as file:
-#     headers = ['Date', 'Hours of Sleep', 'Quality of Sleep', 'Caffeine', 'Comments', 'Other Comments']
-#     outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep': quality_of_sleep, 'Caffeine': caffeine, 'Comments': comments, 'Other Comments': other_comments})
+  with open('user_information.csv', 'a', newline='') as file:
+    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep', 'Caffeine', 'Comments', 'Other Comments']
+    outputDictWriter = csv.DictWriter(file, headers)
+    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep': quality_of_sleep, 'Caffeine': caffeine, 'Comments': comments, 'Other Comments': other_comments})
 
 def sleep_tip():
   with open("sleep_tips.csv", "r") as f:
@@ -40,11 +31,9 @@ def sleep_tip():
     print(f'Fun Sleep Tip: {random.choice(sleep_tips)}')
 
 def filtered_sleep_input():
-  pass
-
-
+  pd.read_csv()
 
 # user_input()
 # # write_user_input()
-sleep_tip()
-# filtered_sleep_input()
+# sleep_tip()
+filtered_sleep_input()
