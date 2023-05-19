70632a39741480ae9d3fd1e2e1b2a5ee3f22afa7
jessicacliong <14395@coderacademy.edu.au>
2023-05-19 13:46:09 +1000

    Separated Write Input of Feature 1, Pseudocode Feature 2 with somecode

--- src/main.py
+++ src/main.py
@@ -1,38 +1,50 @@
 import csv
 import datetime
 import random
-import pandas as pd
 
 def user_input():
-  year = int(input('Enter a year of the log: '))
-  month = int(input('Enter a month of the log: '))
-  day = int(input('Enter a date of the log: '))
+  year = int(input('Enter the year of the log (YYYY): '))
+  month = int(input('Enter the month of the log (MM): '))
+  day = int(input('Enter the date of the log (DD): '))
   date = datetime.date(year, month, day)
-  hours_of_sleep = int(input("How many hours did you sleep? ")) 
+  hours_of_sleep = int(input("How many hours did you sleep? "))
   quality_of_sleep = int(input("From a scale of 1 to 10 how would you rate your sleep? "))
   caffeine = input("Did you have any coffee in the afternoon/evening? Enter: (Y/N) ")
-  comments = input("Do you want to enter any other comments? Enter: (Y/N) ")
-  if comments == 'N':
-    other_comments = ('')
+  journal = input("Would you like to enter a sleep journal? Enter: (Y/N) ")
+  if journal == 'N':
+    journal_entry = ('')
     print("Thank you for using Snooze It. Your data has been saved! ")
   else:
-    other_comments = input("Please enter comments: (Press <Enter> to Continue) ")
+    journal_entry = input("Please enter an entry: (Press <Enter> to Continue) ")
     print("Thank you for using Snooze It. Your data has been saved! ")
+  return date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry 
 
+def write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry):
   with open('user_information.csv', 'a', newline='') as file:
-    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep', 'Caffeine', 'Comments', 'Other Comments']
+    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep (1-10)', 'Caffeine Intake', 'Journal', 'Journal Entry']
     outputDictWriter = csv.DictWriter(file, headers)
-    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep': quality_of_sleep, 'Caffeine': caffeine, 'Comments': comments, 'Other Comments': other_comments})
+    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep (1-10)': quality_of_sleep, 'Caffeine Intake': caffeine, 'Journal': journal, 'Journal Entry': journal_entry})
 
 def sleep_tip():
   with open("sleep_tips.csv", "r") as f:
     csv_reader = csv.reader(f)
     sleep_tips = list(csv_reader)[1:]
-    print(f'Fun Sleep Tip: {random.choice(sleep_tips)}')
+    print(f'Sleep Tip: {random.choice(sleep_tips)}')
 
-def filtered_sleep_input():
-  
+def sleep_log_search():
+  user_search = input("Would you like to view your previous sleep logs? Enter: (Y/N) ")
+  if user_input == "Y":
+    date_search = input("Please enter the date (YYYY-MM-DD) of the log you wish to view :")
+
+  else:
+    print("Thank you for using Snooze It. This program will now terminate. ")  
+    
+open csv File
+read data from csv using pandas
+print to terminal for user to read
+
 # user_input()
-# # write_user_input()
-# sleep_tip()
-filtered_sleep_input()
+# write_user_input()
+sleep_tip()
+# sleep_log_search()
+write_user_input(user_input())
