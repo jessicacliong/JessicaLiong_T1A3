c3e046c0243e28b1d60523a70a511d74ceb5a54d
jessicacliong <14395@coderacademy.edu.au>
2023-05-19 14:50:09 +1000

    Improved call function of Feature 1

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import random
+import pandas as pd
 
 def user_input():
   year = int(input('Enter the year of the log (YYYY): '))
@@ -17,13 +18,13 @@ def user_input():
   else:
     journal_entry = input("Please enter an entry: (Press <Enter> to Continue) ")
     print("Thank you for using Snooze It. Your data has been saved! ")
-  return date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry 
+  return date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry
 
 def write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry):
   with open('user_information.csv', 'a', newline='') as file:
-    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep (1-10)', 'Caffeine Intake', 'Journal', 'Journal Entry']
+    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep (1-10)', 'Caffeine Intake', 'Write Journal', 'Journal Entry']
     outputDictWriter = csv.DictWriter(file, headers)
-    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep (1-10)': quality_of_sleep, 'Caffeine Intake': caffeine, 'Journal': journal, 'Journal Entry': journal_entry})
+    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep (1-10)': quality_of_sleep, 'Caffeine Intake': caffeine, 'Write Journal': journal, 'Journal Entry': journal_entry})
 
 def sleep_tip():
   with open("sleep_tips.csv", "r") as f:
@@ -31,20 +32,19 @@ def sleep_tip():
     sleep_tips = list(csv_reader)[1:]
     print(f'Sleep Tip: {random.choice(sleep_tips)}')
 
-def sleep_log_search():
-  user_search = input("Would you like to view your previous sleep logs? Enter: (Y/N) ")
-  if user_input == "Y":
-    date_search = input("Please enter the date (YYYY-MM-DD) of the log you wish to view :")
-
-  else:
-    print("Thank you for using Snooze It. This program will now terminate. ")  
+# def log_search():
+#   search_request = input("Would you like to view your previous sleep logs? Enter: (Y/N) ")
+#   if user_input == "Y":
+#     date_log = input("Please enter the date (YYYY-MM-DD) of the log you wish to view :")
+#   else:
+#     print("Thank you for using Snooze It. This program will now terminate. ")  
     
-open csv File
-read data from csv using pandas
-print to terminal for user to read
+# open csv File
+# read data from csv using pandas
+# print to terminal for user to read
 
-# user_input()
-# write_user_input()
+date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry = user_input()
+write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry)
 sleep_tip()
-# sleep_log_search()
-write_user_input(user_input())
+# log_search()
+
