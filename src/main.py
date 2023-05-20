26731fe56d8c08f41b2ae6e41f0c17b8f6cd65b7
jessicacliong <14395@coderacademy.edu.au>
2023-05-20 15:28:23 +1000

    Working Raw Feature 2

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import date, datetime
+import datetime as dt
 import random
 import pandas as pd
 
@@ -7,6 +7,7 @@ import pandas as pd
 #   year = int(input("Enter the year of the log (YYYY): "))
 #   month = int(input("Enter the month of the log (MM): "))
 #   day = int(input("Enter the date of the log (DD): "))
+#   date = int(input("Enter the date of the log: (MM/DD/YYYY): "))
 #   date = datetime.date(year, month, day)
 #   hours_of_sleep = int(input("How many hours did you sleep? "))
 #   quality_of_sleep = int(input("From a scale of 1 to 10 how would you rate your sleep? "))
@@ -35,24 +36,23 @@ import pandas as pd
 # def log_search():
 search_request = input("Would you like to view your previous sleep logs? Enter: (Y/N) ")
 if search_request == "Y":
-  year = int(input("Enter the year of the log (YYYY): "))
-  month = int(input("Enter the month of the log (MM): "))
-  day = int(input("Enter the date of the log (DD): "))
-  date = datetime.date(year, month, day)
-  df = pd.read_csv("user_information.csv")
-  df_filtered = df.loc['date']
+  df = pd.read_csv("user_information.csv") # read csv file
+  df['Log Date'] = pd.to_datetime(df['Log Date'])
+  search_date = input("Enter the date of the log (DD-MM-YYYY): ")
+  date_format = dt.datetime.strptime(search_date, "%d-%m-%Y")
+  filtered_df = df[df['Log Date'] == date_format]
 else:
+  filtered_df = (" ")
   print("Thank you for using Snooze It! Come back soon!")
+# return filtered_df
 
-  # else:
-  #   print("Thank you for using Snooze It. Hope to see you again soon! ")  
-    
-# open csv File
-# read data from csv using pandas
-# print to terminal for user to read
+# open csv File using pandas dataframe
+# read data from csv using pandas dataframe
+# print row of information of that date to terminal for user to read
 
 # date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry = user_input()
 # write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry)
 # sleep_tip()
+# filtered_df = log_search()
 print(filtered_df)
 
