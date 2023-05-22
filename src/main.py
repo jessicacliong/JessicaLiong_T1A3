90179b3012d8e8a5825120e64df41e296d3937ae
jessicacliong <14395@coderacademy.edu.au>
2023-05-22 15:49:48 +1000

    Feature 2 Added & Completed 1 week Sleep Log Search

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import csv
 import datetime as dt
 import random
 import pandas as pd
+from simple_term_menu import TerminalMenu
 
 def user_input():
   date = int(input("Enter the date of the log (DD): "))
@@ -16,15 +17,15 @@ def user_input():
     journal_entry = ('')
     print("Thank you for using Snooze It. Your data has been saved! ")
   if journal == 'Y':
-    journal_entry = input('Please enter an entry: (Press <Enter> to Continue) ')
+    journal_entry = input('Please write your entry: (Press <Enter> to Continue) ')
     print("Thank you for using Snooze It. Your data has been saved! ")
   return date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry
 
 def write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry):
   with open('user_information.csv', 'a', newline='') as file:
-    headers = ['Date', 'Hours of Sleep', 'Quality of Sleep (1-10)', 'Caffeine Intake', 'Write Journal', 'Journal Entry']
+    headers = ['Log Date', 'Hours of Sleep', 'Quality of Sleep (1-10)', 'Caffeine Intake', 'Write Journal', 'Journal Entry']
     outputDictWriter = csv.DictWriter(file, headers)
-    outputDictWriter.writerow({'Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep (1-10)': quality_of_sleep, 'Caffeine Intake': caffeine, 'Write Journal': journal, 'Journal Entry': journal_entry})
+    outputDictWriter.writerow({'Log Date': date, 'Hours of Sleep': hours_of_sleep, 'Quality of Sleep (1-10)': quality_of_sleep, 'Caffeine Intake': caffeine, 'Write Journal': journal, 'Journal Entry': journal_entry})
 
 def sleep_tip():
   with open("sleep_tips.csv", "r") as f:
@@ -32,22 +33,28 @@ def sleep_tip():
     sleep_tips = list(csv_reader)[1:]
     print(f'Sleep Tip: {random.choice(sleep_tips)}')
 
-def log_search():
-  search_request = input("Would you like to view your previous sleep logs? Enter: (Y/N) ")
-  if search_request == "Y":
-    df = pd.read_csv("user_information.csv") 
-    df['Log Date'] = pd.to_datetime(df['Log Date'])
-    search_date = input("Enter the date of the log (DD-MM-YYYY): ")
-    date_format = dt.datetime.strptime(search_date, "%d-%m-%Y")
-    filtered_df = df[df['Log Date'] == date_format]
-    print(filtered_df)
-    print("Thank you for using Snooze It! Please come back soon!")
-  if search_request == "N":
-    filtered_df = ('')
-    print("Thank you for using Snooze It! Please come back soon!")
+def log_search_day():
+  df = pd.read_csv("user_information.csv") 
+  df['Log Date'] = pd.to_datetime(df['Log Date']) # converts data type into a datetime64[ns] object
+  search_date = input("Enter the date of the log (DD-MM-YYYY): ")
+  filtered_df = df[df['Log Date'] == search_date] # both the csv file and user input format are in strings, so don't need to convert to datetime object to compare the two inputs
+  print(filtered_df)
 
+def log_search_week():
+  df = pd.read_csv("user_information.csv") 
+  df['Log Date'] = pd.to_datetime(df['Log Date']) # converts data type into a datetime64[ns] object
+  start_date = input("Enter the start date of the log (YYYY-MM-DD): ")
+  end_date = input("Enter the last date of the log (YYYY-MM-DD): ")
+  mask = (df['Log Date'] >= start_date) & (df['Log Date'] <= end_date)
+  df2 = df.loc[mask]
+  print(df2)
+
+def end_application():
+  print("Thank you for using Snooze It! Please come back again soon!")
 
 date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry = user_input()
 write_user_input(date, hours_of_sleep, quality_of_sleep, caffeine, journal, journal_entry)
 sleep_tip()
-log_search()
+log_search_day()
+log_search_week()
+end_application()
